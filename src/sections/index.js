be469915f56ad46cea20eb38cb1aa7c0de0c3bd3
aths7 <atharvaw007@gmail.com>
2025-02-07 10:03:47 +0530

    initial commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import PopularProducts from "./PopularProducts";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+export {
+  Hero,
+  Services,
+  SpecialOffer,
+  PopularProducts,
+  Footer,
+  CustomerReviews,
+  Subscribe,
+  SuperQuality,
+};
